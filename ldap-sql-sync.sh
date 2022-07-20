f4f6107fb0bf0b396a69129855e67a6b537d5a10
iqzirr <rizqi.fireman@gmail.com>
2022-07-20 15:08:38 +0700

    Update: multiple schemas support

--- ldap-sql-sync.sh
+++ ldap-sql-sync.sh
@@ -4,19 +4,35 @@ usercount=$(ldapsearch -x -D $LDAP_USER -h $LDAP_HOST -p $LDAP_PORT -w $LDAP_PAS
 i=1
 while [[ $i -le $usercount ]]
 do
-    username[$i]=$(ldapsearch -x -D $LDAP_USER -h $LDAP_HOST -p $LDAP_PORT -w $LDAP_PASS -LLL "database=*" database databasePermission |  tr "," "\n"  | grep dn | awk '{print $2}' | cut -d "=" -f 2)
-    database[$i]=$(ldapsearch -x -D $LDAP_USER -h $LDAP_HOST -p $LDAP_PORT -w $LDAP_PASS -LLL "database=*" database | grep database | sed -n $((i))p | awk '{print $2}')
+    username[$i]=$(ldapsearch -x -D $LDAP_USER -h $LDAP_HOST -p $LDAP_PORT -w $LDAP_PASS -LLL "database=*" database databasePermission |  tr "," "\n"  | grep dn | sed -n $((i))p | awk '{print $2}' | cut -d "=" -f 2)
+    dbcount[$i]=$(ldapsearch -x -D $LDAP_USER -h $LDAP_HOST -p $LDAP_PORT -w $LDAP_PASS -LLL "database=*" database | grep database | sed -n $((i))p | awk '{print $2}'|  tr "," "\n" | wc -l)
+
+    j=1
+    while [[ $j -le ${dbcount[$i]} ]]
+    do
+        database[$i,$j]=$(ldapsearch -x -D $LDAP_USER -h $LDAP_HOST -p $LDAP_PORT -w $LDAP_PASS -LLL "database=*" database | grep database | sed -n $((i))p | awk '{print $2}'|  tr "," "\n" | sed -n $((j))p)
+        ((j++))
+    done
+
     databasePermission[$i]=$(ldapsearch -x -D $LDAP_USER -h $LDAP_HOST -p $LDAP_PORT -w $LDAP_PASS -LLL "database=*" databasePermission | grep databasePermission | sed -n $((i))p | awk '{print $2}')
     ((i++))
 done
 
-counter=1
-while [[ $counter -le $((usercount)) ]]
+usercounter=1
+while [[ $usercounter -le $usercount ]]
 do
-        user=${username[$counter]}
-        db=${database[$counter]}
-        grant=${databasePermission[$counter]}
+        #Create the User if not exists
+        user=${username[$usercounter]}
         mariadb -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS -P $MYSQL_PORT -e "CREATE USER IF NOT EXISTS '$user'@'%' IDENTIFIED VIA pam USING 'mariadb'"
-        mariadb -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS -P $MYSQL_PORT -e "GRANT $grant ON $db.* TO '$user'@'%' ; FLUSH PRIVILEGES"
-        ((counter++))
+
+        #Create the DB Privileges
+        dbcounter=1
+        while [[ $dbcounter -le ${dbcount[$usercounter]} ]]
+        do
+                db=${database[$usercounter,$dbcounter]}
+                grant=${databasePermission[$usercounter]}
+                mariadb -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS -P $MYSQL_PORT -e "GRANT $grant ON $db.* TO '$user'@'%' ; FLUSH PRIVILEGES"
+                ((dbcounter++))
+        done
+        ((usercounter++))
 done
